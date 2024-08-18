4acfd277e644f14d9099efbc0933486b913e04f5
VoidMinus <voidminus.creates@yahoo.com>
2024-08-17 22:59:15 -0500

    Add first fluorite items and itemgroups

--- src/main/java/net/voidminus/mccourse/MCCourseMod.java
+++ src/main/java/net/voidminus/mccourse/MCCourseMod.java
@@ -2,14 +2,18 @@ package net.voidminus.mccourse;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.voidminus.mccourse.item.ModItemGroups;
+import net.voidminus.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MCCourseMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("mccourse");
+	public static final String MOD_ID = "mccourse";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
+		ModItemGroups.registerItemGroups();
 	}
 }
