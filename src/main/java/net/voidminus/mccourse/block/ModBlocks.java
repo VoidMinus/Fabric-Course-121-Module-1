d06c9bf864982b85acc8d92e31eb81b93e903f3d
VoidMinus <voidminus.creates@yahoo.com>
2024-08-18 07:27:58 -0500

    Add magic block

--- src/main/java/net/voidminus/mccourse/block/ModBlocks.java
+++ src/main/java/net/voidminus/mccourse/block/ModBlocks.java
@@ -11,6 +11,7 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.voidminus.mccourse.MCCourseMod;
+import net.voidminus.mccourse.block.custom.MagicBlock;
 
 public class ModBlocks {
 
@@ -33,6 +34,9 @@ public class ModBlocks {
     public static final Block FLUORITE_END_ORE = registerBlock("fluorite_end_ore",
             new ExperienceDroppingBlock(UniformIntProvider.create(3,6),
                     AbstractBlock.Settings.create().strength(6f).requiresTool()));
+
+    public static final Block MAGIC_BLOCK = registerBlock("magic_block",
+            new MagicBlock(AbstractBlock.Settings.create().strength(1f).requiresTool()));
 
     private static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
