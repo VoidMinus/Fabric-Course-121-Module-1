cab622beede65eea2c70ed604b45e46f92e85fe9
VoidMinus <voidminus.creates@yahoo.com>
2024-08-19 10:29:34 -0500

    Add tooltips

--- src/main/java/net/voidminus/mccourse/item/ModItems.java
+++ src/main/java/net/voidminus/mccourse/item/ModItems.java
@@ -4,11 +4,16 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.voidminus.mccourse.MCCourseMod;
 import net.voidminus.mccourse.item.custom.ChainsawItem;
+
+import java.util.List;
 
 public class ModItems {
     public static final Item FLUORITE = registerItem("fluorite",
@@ -21,7 +26,13 @@ public class ModItems {
             new ChainsawItem(new Item.Settings().maxDamage(32)));
 
     public static final Item STRAWBERRY = registerItem("strawberry",
-            new Item(new Item.Settings().food(ModFoodComponents.STRAWBERRY)));
+            new Item(new Item.Settings().food(ModFoodComponents.STRAWBERRY)){
+                @Override
+                public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType type) {
+                    tooltip.add(Text.translatable("tooltip.mccourse.strawberry.tooltip.1"));
+                    super.appendTooltip(stack, context, tooltip, type);
+                }
+            });
 
     public static final Item STARLIGHT_ASHES = registerItem("starlight_ashes",
             new Item(new Item.Settings()));
